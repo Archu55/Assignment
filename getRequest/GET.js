13d231029e993dcd49c82a3e70981038f91bd352
Archu55 <47347174+Archu55@users.noreply.github.com>
2019-02-05 13:44:13 +0530

    Create GET.js

---
+++ getRequest/GET.js
@@ -0,0 +1,22 @@
+var https = require('https');
+var assert = require('assert');
+
+https.get(' https://jsonplaceholder.typicode.com/posts/1',
+    res => {
+        console.log(res.statusCode);
+        function assertStatue() {
+            return res.statusCode === 200
+        }
+        console.log(assertStatue());
+        let body = '';
+        res.on('data', data => {
+            body += data;
+            let info = JSON.parse(body);
+            function verifyid() {
+                return info.userId === 1
+            }
+            console.log(verifyid());
+        })
+        res.on('end', () => console.log(body));
+
+    });
